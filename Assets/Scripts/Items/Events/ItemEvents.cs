ddc49d1c3b2500018f3e10e42eb5a3bc28d96d39
Seho-P <qkrtpgh77@gmail.com>
2026-01-30 20:30:02 +0900

    아이템 장착 스탯 변화 디버그 툴 수정

--- Assets/Scripts/Items/Events/ItemEvents.cs
+++ Assets/Scripts/Items/Events/ItemEvents.cs
@@ -1,25 +1,29 @@
 using System;
+using System.Collections.Generic;
 using Items.Data;
+using Items.Enums;
 
 namespace Items.Events
 {
     public static class ItemEvents
 {
-    public static event Action<ItemInstance> OnItemEquipped;
-    public static event Action<ItemInstance> OnItemUnequipped;
+    /// <summary>아이템 장착 시 (장착된 아이템, 장착 직전 스탯 스냅샷)</summary>
+    public static event Action<ItemInstance, Dictionary<StatType, float>> OnItemEquipped;
+    /// <summary>아이템 해제 시 (해제된 아이템, 해제 직전 스탯 스냅샷)</summary>
+    public static event Action<ItemInstance, Dictionary<StatType, float>> OnItemUnequipped;
     public static event Action<string, int> OnSetCountChanged; // 세트명, 현재 개수
     public static event Action<string, int> OnSetComplete; // 세트명, 완성된 개수
     public static event Action OnStatsChanged; // 스탯 변경 시
 
-    public static void InvokeItemEquipped(ItemInstance item)
+    public static void InvokeItemEquipped(ItemInstance item, Dictionary<StatType, float> statsBefore)
     {
-        OnItemEquipped?.Invoke(item);
+        OnItemEquipped?.Invoke(item, statsBefore);
         OnStatsChanged?.Invoke();
     }
 
-    public static void InvokeItemUnequipped(ItemInstance item)
+    public static void InvokeItemUnequipped(ItemInstance item, Dictionary<StatType, float> statsBefore)
     {
-        OnItemUnequipped?.Invoke(item);
+        OnItemUnequipped?.Invoke(item, statsBefore);
         OnStatsChanged?.Invoke();
     }
 
